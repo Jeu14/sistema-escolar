8d5d49ec4a2dc297ad96d247b238d1e649b39248
DESKTOP-H9UVA2A\User <jedissonsantana34@gmail.com>
2024-02-07 19:28:05 -0300

    first commit

---
+++ SQL/dumb.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE sistemaescolar;
+
+CREATE TABLE usuarios (
+	id SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  senha VARCHAR(100) NOT NULL,
+  materia VARCHAR(100)
+);
---
+++ src/controllers/userRegistration.js
@@ -0,0 +1,47 @@
+const knex = require("../database/database");
+const bcrypt = require("bcrypt");
+const {
+    schemaUserRegistration,
+} = require("../validations/userRegistrationValidation");
+
+const registerUser = async (req, res) => {
+  const { error, value } = schemaUserRegistration.validate(req.body);
+
+  if (error) {
+    const errorMessage = error.details[0].message;
+    return res.status(400).json({ erro: errorMessage });
+  }
+
+  const { nome, email, senha } = value;
+
+  try {
+    const checkEmail = await knex("usuarios")
+      .where("email", email)
+      .first();
+    if (checkEmail) {
+      return res.status(400).json({ erro: "E-mail já cadastrado" });
+    }
+
+    const hashPass = await bcrypt.hash(senha, 10);
+
+    const registeredUser = await knex("usuarios")
+      .insert({
+        nome,
+        email,
+        senha: hashPass
+      })
+      .returning(["nome", "email"]);
+
+    return res
+      .status(201)
+      .json({
+        message: "Usuário cadastrado com sucesso",
+        usuario: registeredUser[0],
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ erro: "Erro interno do servidor" });
+  }
+};
+
+module.exports = { registerUser };
